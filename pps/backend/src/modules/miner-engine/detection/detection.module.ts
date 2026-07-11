a57620178b2f8fb4657880a96fa75d6bb873ad89
Azzymage <azhar.azhar9999@Gmail.com>
2026-07-11 17:07:54 +0530

    Delete pps/backend/src/modules/miner-engine/detection/detection.module.ts

--- pps/backend/src/modules/miner-engine/detection/detection.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DetectionService } from './detection.service';
-
-@Module({
-  providers: [DetectionService],
-  exports: [DetectionService],
-})
-export class DetectionModule {}
